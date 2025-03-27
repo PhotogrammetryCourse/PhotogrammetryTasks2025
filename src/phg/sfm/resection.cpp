c13ccbe401f9caa4b42e3cc6be58cb0f4859db44
XyliganSereja <matva0509@gmail.com>
2025-03-27 22:41:47 +0300

    add part of solution

--- src/phg/sfm/resection.cpp
+++ src/phg/sfm/resection.cpp
@@ -4,8 +4,12 @@
 #include "sfm_utils.h"
 #include <Eigen/SVD>
 #include <iostream>
+#include <cstdint>
+#include <cstddef>
 
 namespace {
+
+  constexpr int32_t TW = 12;
 
 // Сделать из первого минора 3х3 матрицу вращения, скомпенсировать масштаб у
 // компоненты сдвига
@@ -22,15 +26,16 @@ matrix34d canonicalizeP(const matrix34d &P) {
   }
 
   double sc = 0;
-  for (int i = 0; i < 9; i++) {
-    sc += RR.val[i] * RR.val[i];
+  for (const auto i : RR.val) {
+    sc += i * i;
   }
+
   sc = std::sqrt(3 / sc);
 
   Eigen::MatrixXd RRe;
   copy(RR, RRe);
-  Eigen::JacobiSVD<Eigen::MatrixXd> svd(RRe, Eigen::ComputeFullU |
-                                                 Eigen::ComputeFullV);
+  const Eigen::JacobiSVD svd(RRe, Eigen::ComputeFullU |
+                                  Eigen::ComputeFullV);
   RRe = svd.matrixU() * svd.matrixV().transpose();
   copy(RRe, RR);
 
@@ -40,6 +45,7 @@ matrix34d canonicalizeP(const matrix34d &P) {
   for (int i = 0; i < 9; ++i) {
     result(i / 3, i % 3) = RR(i / 3, i % 3);
   }
+
   result(0, 3) = tt(0);
   result(1, 3) = tt(1);
   result(2, 3) = tt(2);
@@ -50,30 +56,28 @@ matrix34d canonicalizeP(const matrix34d &P) {
 // (см. Hartley & Zisserman p.178)
 cv::Matx34d estimateCameraMatrixDLT(const cv::Vec3d *Xs, const cv::Vec3d *xs,
                                     int count) {
-  throw std::runtime_error("not implemented yet");
-  //        using mat = Eigen::MatrixXd;
-  //        using vec = Eigen::VectorXd;
-  //
-  //        mat A(TODO);
-  //
-  //        for (int i = 0; i < count; ++i) {
-  //
-  //            double x = xs[i][0];
-  //            double y = xs[i][1];
-  //            double w = xs[i][2];
-  //
-  //            double X = Xs[i][0];
-  //            double Y = Xs[i][1];
-  //            double Z = Xs[i][2];
-  //            double W = 1.0;
-  //
-  //            TODO
-  //        }
-  //
-  //        matrix34d result;
-  //          TODO
-  //
-  //        return canonicalizeP(result);
+  const int32_t rows = count * 2;
+  constexpr int32_t cols = TW;
+  Eigen::MatrixXd A(rows, cols);
+  const Eigen::RowVector4d fz = Eigen::VectorXd::Zero(4);
+
+  for (size_t i = 0; i < count; ++i) {
+    const double x = xs[i][0];
+    double y = xs[i][1];
+    double z = xs[i][2];
+    Eigen::RowVector4d X(Xs[i][0], Xs[i][1], Xs[i][2], 1.0f);
+    A.row(2 * i) << fz, -z * X, y * X;
+    A.row(2 * i + 1) << z * X, fz, -x * X;
+  }
+
+  const Eigen::JacobiSVD svd(A, Eigen::ComputeFullV);
+  Eigen::VectorXd ft = svd.matrixV().col(cols - 1);
+  matrix34d res;
+  for (size_t i = 0; i < TW; ++i) {
+    res(i / 4, i % 4) = ft[i];
+  }
+
+  return canonicalizeP(res);
 }
 
 // По трехмерным точкам и их проекциям на изображении определяем положение
@@ -81,70 +85,68 @@ cv::Matx34d estimateCameraMatrixDLT(const cv::Vec3d *Xs, const cv::Vec3d *xs,
 cv::Matx34d estimateCameraMatrixRANSAC(const phg::Calibration &calib,
                                        const std::vector<cv::Vec3d> &X,
                                        const std::vector<cv::Vec2d> &x) {
-  throw std::runtime_error("not implemented yet");
-  //        if (X.size() != x.size()) {
-  //            throw std::runtime_error("estimateCameraMatrixRANSAC: X.size()
-  //            != x.size()");
-  //        }
-  //
-  //        const int n_points = X.size();
-  //
-  //        // https://en.wikipedia.org/wiki/Random_sample_consensus#Parameters
-  //        // будет отличаться от случая с гомографией
-  //        const int n_trials = TODO;
-  //
-  //        const double threshold_px = 3;
-  //
-  //        const int n_samples = TODO;
-  //        uint64_t seed = 1;
-  //
-  //        int best_support = 0;
-  //        cv::Matx34d best_P;
-  //
-  //        std::vector<int> sample;
-  //        for (int i_trial = 0; i_trial < n_trials; ++i_trial) {
-  //            phg::randomSample(sample, n_points, n_samples, &seed);
-  //
-  //            cv::Vec3d ms0[n_samples];
-  //            cv::Vec3d ms1[n_samples];
-  //            for (int i = 0; i < n_samples; ++i) {
-  //                ms0[i] = TODO;
-  //                ms1[i] = TODO;
-  //            }
-  //
-  //            cv::Matx34d P = estimateCameraMatrixDLT(ms0, ms1, n_samples);
-  //
-  //            int support = 0;
-  //            for (int i = 0; i < n_points; ++i) {
-  //                cv::Vec2d px = TODO спроецировать 3Д точку в пиксель с
-  //                использованием P и calib; if (cv::norm(px - x[i]) <
-  //                threshold_px) {
-  //                    ++support;
-  //                }
-  //            }
-  //
-  //            if (support > best_support) {
-  //                best_support = support;
-  //                best_P = P;
-  //
-  //                std::cout << "estimateCameraMatrixRANSAC : support: " <<
-  //                best_support << "/" << n_points << std::endl;
-  //
-  //                if (best_support == n_points) {
-  //                    break;
-  //                }
-  //            }
-  //        }
-  //
-  //        std::cout << "estimateCameraMatrixRANSAC : best support: " <<
-  //        best_support << "/" << n_points << std::endl;
-  //
-  //        if (best_support == 0) {
-  //            throw std::runtime_error("estimateCameraMatrixRANSAC : failed to
-  //            estimate camera matrix");
-  //        }
-  //
-  //        return best_P;
+  if (X.size() != x.size()) {
+      throw std::runtime_error("estimateCameraMatrixRANSAC: X.size() != x.size()");
+  }
+
+  const int n_points = X.size();
+
+  // https://en.wikipedia.org/wiki/Random_sample_consensus#Parameters
+  // будет отличаться от случая с гомографией
+  const int n_trials = 1000;
+
+  const double threshold_px = 3;
+
+  const int n_samples = 10;
+  uint64_t seed = 1;
+
+  int best_support = 0;
+  cv::Matx34d best_P;
+
+  std::vector<int> sample;
+  for (int i_trial = 0; i_trial < n_trials; ++i_trial) {
+      phg::randomSample(sample, n_points, n_samples, &seed);
+
+      cv::Vec3d ms0[n_samples];
+      cv::Vec3d ms1[n_samples];
+      for (int i = 0; i < n_samples; ++i) {
+          ms0[i] = X[sample[i]];
+          ms1[i] = calib.unproject(x[sample[i]]);
+      }
+
+      cv::Matx34d P = estimateCameraMatrixDLT(ms0, ms1, n_samples);
+
+      int support = 0;
+      for (int i = 0; i < n_points; ++i) {
+          cv::Vec4d X4(X[i][0], X[i][1], X[i][2], 1);
+          cv::Vec3d proj = calib.project(P * X4);
+
+          if (auto px = cv::Vec2d(proj[0] / proj[2], proj[1] / proj[2]); cv::norm(px - x[i]) < threshold_px) {
+              ++support;
+          }
+      }
+
+      if (support > best_support) {
+          best_support = support;
+          best_P = P;
+
+          std::cout << "estimateCameraMatrixRANSAC : support: " <<
+          best_support << "/" << n_points << std::endl;
+
+          if (best_support == n_points) {
+              break;
+          }
+      }
+  }
+
+  std::cout << "estimateCameraMatrixRANSAC : best support: " <<
+  best_support << "/" << n_points << std::endl;
+
+  if (best_support == 0) {
+      throw std::runtime_error("estimateCameraMatrixRANSAC : failed to estimate camera matrix");
+  }
+
+  return best_P;
 }
 
 } // namespace
--- src/phg/sfm/sfm_utils.cpp
+++ src/phg/sfm/sfm_utils.cpp
@@ -20,9 +20,8 @@ void phg::randomSample(std::vector<int> &dst, int max_id, int sample_size,
                        uint64_t *state) {
   dst.clear();
 
-  const int max_attempts = 1000;
-
   for (int i = 0; i < sample_size; ++i) {
+    constexpr int max_attempts = 1000;
     for (int k = 0; k < max_attempts; ++k) {
       int v = xorshift64(state) % max_id;
       if (dst.empty() || std::find(dst.begin(), dst.end(), v) == dst.end()) {
@@ -39,5 +38,8 @@ void phg::randomSample(std::vector<int> &dst, int max_id, int sample_size,
 // проверяет, что расстояние от точки до линии меньше порога
 bool phg::epipolarTest(const cv::Vec2d &pt0, const cv::Vec2d &pt1,
                        const cv::Matx33d &F, double t) {
-  throw std::runtime_error("not implemented yet");
+  cv::Vec3d l = F * cv::Vec3d(pt0[0], pt0[1], 1.0f);
+  const double n2 = l[0] * l[0] + l[1] * l[1];
+  const double d = l[0] * pt1[0] + l[1] * pt1[1] + l[2];
+  return d * d < t * t * n2;
 }
