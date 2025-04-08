aa6e6bd2559853e201bb785c278b38645d8075c4
XyliganSereja <matva0509@gmail.com>
2025-04-09 02:47:25 +0300

    add solution

--- src/phg/mvs/depth_maps/pm_geometry.h
+++ src/phg/mvs/depth_maps/pm_geometry.h
@@ -2,20 +2,22 @@
 
 #include "pm_fast_random.h"
 
-#include <phg/sfm/defines.h>
 #include <opencv2/core/matx.hpp>
-
+#include <phg/sfm/defines.h>
 
 namespace phg {
 
-    vector4d homogenize(const vector3d &p);
-    
-    double dot(const vector3d &a, const vector3d &b);
-    
-    float norm2(const vector3f &v);
+vector4d homogenize(const vector3d &p);
 
-    vector3f randomNormalObservedFromCamera(const matrix3d &RtoWorld, FastRandom &r);
+double dot(const vector3d &a, const vector3d &b);
 
-    bool intersectWithPlane(const vector3d &plane_point, const vector3d &plane_normal, const vector3d &ray_org, const vector3d &ray_dir, vector3d &global_intersection);
-    
-}
+float norm2(const vector3f &v);
+
+vector3f randomNormalObservedFromCamera(const matrix3d &RtoWorld,
+                                        FastRandom &r);
+
+bool intersectWithPlane(const vector3d &plane_point,
+                        const vector3d &plane_normal, const vector3d &ray_org,
+                        const vector3d &ray_dir, vector3d &global_intersection);
+
+} // namespace phg
